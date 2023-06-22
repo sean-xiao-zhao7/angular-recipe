237da5cd83b9adce667de2a40835c6f3d821280e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-22 15:08:49 -0400

    Add all models

---
+++ src/app/models/ingredient.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  public name: string;
+  public longDescription: string;
+  public imageUrl: string;
+
+  constructor(name: string, longDescription: string, imageUrl: string) {
+    this.name = name;
+    this.longDescription = longDescription;
+    this.imageUrl = imageUrl;
+  }
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  public firstName: string;
+  public lastName: string;
+  public username: string;
+
+  constructor(firstName: string, lastName: string, username: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+  }
+}
