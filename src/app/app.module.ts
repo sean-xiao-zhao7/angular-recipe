e8d0677e294a827ad332dfca17b3ba4e5ee79a45
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-22 14:54:11 -0400

    Fix recipe-book import paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
-import { RecipeItemDetailsComponent } from './components/recipe-item-details/recipe-item-details.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipe-book/recipe-item/recipe-item.component';
+import { RecipeItemDetailsComponent } from './components/recipe-book/recipe-item-details/recipe-item-details.component';
 import { ShopListComponent } from './components/shop/shop-list/shop-list.component';
 import { ShopListEditComponent } from './components/shop/shop-list-edit/shop-list-edit.component';
 import { IngredientItemComponent } from './components/shop/ingredient-item/ingredient-item.component';
@@ -18,13 +18,10 @@ import { IngredientItemComponent } from './components/shop/ingredient-item/ingre
     RecipeItemDetailsComponent,
     ShopListComponent,
     ShopListEditComponent,
-    IngredientItemComponent
+    IngredientItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
