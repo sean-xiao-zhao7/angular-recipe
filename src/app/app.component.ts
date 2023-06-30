2a83229c547df5e0e21c6d62c26825bea4b8dad9
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-30 18:03:04 -0400

    Add shop service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Ingredient } from './models/ingredient';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +8,6 @@ import { Ingredient } from './models/ingredient';
 export class AppComponent {
   title = 'My Recipes';
   section = 'shop';
-
-  // available contents
-  allIngredients = [
-    new Ingredient('Ingredient 1', '', ''),
-    new Ingredient('Ingredient 2', '', ''),
-    new Ingredient('Ingredient 3', '', ''),
-  ];
 
   onChangeSection(section: string) {
     this.section = section;
--- src/app/services/shop.service.ts
+++ src/app/services/shop.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../models/ingredient';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
+  private allIngredients = [
+    new Ingredient('Ingredient 1', '', ''),
+    new Ingredient('Ingredient 2', '', ''),
+    new Ingredient('Ingredient 3', '', ''),
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getAllIngredients() {
+    return this.allIngredients.slice();
+  }
 }
