5446e9640fb77a2c3447ec31227df0fd172d59e3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-06 15:10:15 -0400

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopListComponent } from './components/shop/shop-list/shop-list.component';
+import { MyIngredientsComponent } from './components/inventory/my-ingredients/my-ingredients.component';
+import { RecipeListComponent } from './components/recipe-book/recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShopListComponent,
+  },
+  {
+    path: 'inventory',
+    component: MyIngredientsComponent,
+  },
+  {
+    path: 'recipe-book',
+    component: RecipeListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
